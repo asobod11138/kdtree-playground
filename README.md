9eeef22cc290c7c7ec65ae07864fa8ceb50a5d41
mano_kazuki <asobod11138@gmail.com>
2024-01-13 12:26:16 +0900

    [add] readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # kdtree plauground
 kdtreeを色々試したい。
 1. kdtree_basic: numpyのみで自前実装。アルゴリズムの理解と速度計測のベースライン
+2. kdtree_open3d: open3dに実装されているKDTreeを使った実装。別のライブラリで良い感じに実装されているものと速度比較したいモチベ。
+
+# 速度比較結果
+以下のコマンドで速度比較ができる。
+```
+make test-benchmark
+```
+
+## 速度の計測対象
+pytest-benchmarksを使って計測
+
+1. KDTreeの近傍点探索速度
+    - 木の生成は含まない
+    - KDTreeに格納されたデータ（3次元5万点）に対して、クエリ（1点）を200種類探索させた速度の計測
+    - 1クエリづつfor文で回しているので、1点の探索を200回するときの速度になっているはず
+2. KDTreeの生成速度
+    - ３次元5万点のデータに対してKDTreeを構築する速度
+
+自分の環境（CPU: i7-13700K, GPU: RTX4080, RAM: 32GB(DDR5-4800), OS: WSL2 Ubuntu22.04）での結果
+![Benchmark Result Image Home PC](readme_assets/kdtree_benchmark_homePC.png)
--- tests/test_speed.py
+++ tests/test_speed.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 
 from kdtree_lib import KDTreeBasic, KDTreeOpen3d
 
+points_num = 50_000
+query_num = 200
 
 def get_random_points(num_points) -> np.ndarray:
     return np.array([(random.uniform(-100, 100), random.uniform(-100, 100), random.uniform(-100, 100)) for _ in range(num_points)])  # noqa: S311
@@ -17,58 +19,50 @@ def build_tree_speed(arg_class, points) -> None:
     arg_class(points)
 
 
-def kdtree_speed(arg_class, tree, querys) -> None:
-    for query in tqdm(querys, desc=f"kdtree speed: {arg_class.__name__}"):
+def kdtree_speed(tree, querys) -> None:
+    for query in querys:
         tree.nearest_neighbor(query)
 
+##########################################
 
 def test_build_tree_kdtree_basic(benchmark) -> None:
     benchmark.group = "Tree Building"
     # テスト用のデータを準備
-    points = get_random_points(1_000)
+    points = get_random_points(points_num)
 
-    # build_treeメソッドの実行速度を計測
-    ret = benchmark.pedantic(
-        build_tree_speed,
-        kwargs={"arg_class": KDTreeBasic, "points": points},  # テスト対象に渡す引数 (キーワード付き)
-        rounds=100,  # テスト対象の呼び出し回数
-        iterations=10,
-    )  # 試行回数
-
-
-def test_nearest_neighbor_kdtree_basic(benchmark) -> None:
-    benchmark.group = "Nearest Neighbor Search"
-    # テスト用のデータを準備
-    points = get_random_points(10_000)
-    tree = KDTreeBasic(points)
-
-    querys = get_random_points(200)
-
-    # nearest_neighborメソッドの実行速度を計測
-    benchmark(kdtree_speed, arg_class=KDTreeBasic, tree=tree, querys=querys)
+    benchmark(build_tree_speed, arg_class=KDTreeBasic, points=points)
 
 
 def test_build_tree_kdtree_open3d(benchmark) -> None:
     benchmark.group = "Tree Building"
     # テスト用のデータを準備
-    points = get_random_points(1_000)
+    points = get_random_points(points_num)
 
-    # build_treeメソッドの実行速度を計測
-    ret = benchmark.pedantic(
-        build_tree_speed,
-        kwargs={"arg_class": KDTreeOpen3d, "points": points},  # テスト対象に渡す引数 (キーワード付き)
-        rounds=100,  # テスト対象の呼び出し回数
-        iterations=10,
-    )  # 試行回数
+    benchmark(build_tree_speed, arg_class=KDTreeOpen3d, points=points)
+
+
+##########################################
+
+def test_nearest_neighbor_kdtree_basic(benchmark) -> None:
+    benchmark.group = "Nearest Neighbor Search"
+    # テスト用のデータを準備
+    points = get_random_points(points_num)
+    tree = KDTreeBasic(points)
+
+    querys = get_random_points(query_num)
+
+    # nearest_neighborメソッドの実行速度を計測
+    benchmark(kdtree_speed, tree=tree, querys=querys)
+
 
 
 def test_nearest_neighbor_kdtree_open3d(benchmark) -> None:
     benchmark.group = "Nearest Neighbor Search"
     # テスト用のデータを準備
-    points = get_random_points(10_000)
-    tree = KDTreeBasic(points)
+    points = get_random_points(points_num)
+    tree = KDTreeOpen3d(points)
 
-    querys = get_random_points(200)
+    querys = get_random_points(query_num)
 
     # nearest_neighborメソッドの実行速度を計測
-    benchmark(kdtree_speed, arg_class=KDTreeOpen3d, tree=tree, querys=querys)
+    benchmark(kdtree_speed, tree=tree, querys=querys)
